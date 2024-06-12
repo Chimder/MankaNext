10eabe8efc23961d7697a24795e592f585a4dca4
Chimas <dimas.kirilyuk@gmail.com>
2024-06-12 21:44:24 +0300

    back

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-if (process.env.NODE_ENV === "development") {
-  const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
-  setupDevPlatform();
-}
-
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  // images: {
-  //   unoptimized: true,
-  // },
 };
 
 module.exports = nextConfig;
--- src/pages/manka/[manka]/index.tsx
+++ src/pages/manka/[manka]/index.tsx
@@ -34,7 +34,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const name = params?.manka as string;
   const data = await getMangaByName({ name: name });
-  return { props: { data } };
+  return { props: { data }, revalidate: 10 };
 };
 
 const Manga = ({ data: manga }: MangaProps) => {
