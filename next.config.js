5bdc177992b11b31d8dbe09e453672d0fb11a06d
Chimas <dimas.kirilyuk@gmail.com>
2024-06-12 20:59:24 +0300

    -export

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 if (process.env.NODE_ENV === "development") {
-  const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev")
-  setupDevPlatform()
+  const { setupDevPlatform } = require("@cloudflare/next-on-pages/next-dev");
+  setupDevPlatform();
 }
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // output: 'export',
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
