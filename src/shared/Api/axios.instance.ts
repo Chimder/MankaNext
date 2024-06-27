53b3631998a64f43021516c4e6b5e658f1b6c1aa
Chimder <dimas.kirilyuk@gmail.com>
2024-06-27 11:08:20 +0000

    fix- orval chi go

--- src/shared/Api/axios.instance.ts
+++ src/shared/Api/axios.instance.ts
@@ -11,7 +11,7 @@ export const AXIOS_INSTANCE = Axios.create({
 
 export const customInstance = <T>(
   config: AxiosRequestConfig,
-  options?: AxiosRequestConfig
+  options?: AxiosRequestConfig,
 ): Promise<T> => {
   // const source = Axios.CancelToken.source();
   return AXIOS_INSTANCE({
