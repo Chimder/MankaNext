35c7eca921261466153c80d7b56d6ca4486eba0d
Chimder <dimas.kirilyuk@gmail.com>
2024-07-04 11:19:57 +0000

    res

--- src/pages/api/delete-cookie.ts
+++ src/pages/api/delete-cookie.ts
@@ -5,6 +5,24 @@ export default async function deleteCookie(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  console.log("Request cookies:", req.cookies);
+
   if (!req.headers.cookie) {
     res.status(400).json({ error: "No cookies in request" });
     return;
@@ -27,6 +45,8 @@ export default async function deleteCookie(
       httpOnly: false,
     }),
   );
+
+  console.log("Cookie deleted");
 
   res.status(200).json({ message: "Cookie deleted" });
 }
